85985103ed377065e71cad031899cbf16c560811
Damir Mansurov <rave9373@gmail.com>
2020-08-26 12:23:37 +0300

    Updates

---
+++ precache-manifest.65c810fd631cb829ead5614e1681e963.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "489aa98fda0c785f2d86",
+    "url": "/test/static/css/main.073831cc.chunk.css"
+  },
+  {
+    "revision": "489aa98fda0c785f2d86",
+    "url": "/test/static/js/main.8dea0c6d.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "758821a751ad092b1277",
+    "url": "/test/static/css/2.de46e4a1.chunk.css"
+  },
+  {
+    "revision": "758821a751ad092b1277",
+    "url": "/test/static/js/2.da9c25ca.chunk.js"
+  },
+  {
+    "revision": "66b5cf2055d3299c96c3d01c2006533a",
+    "url": "/test/static/media/cat_photo.66b5cf20.png"
+  },
+  {
+    "revision": "d7b41a883e081c64e2e69c241d0ace05",
+    "url": "/test/index.html"
+  }
+];
